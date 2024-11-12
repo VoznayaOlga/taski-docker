97228cc77e126a6b536e327debfa4ccf3c26253c
Olga Voznaya <voznaya.olga@yandex.ru>
2024-11-12 19:44:24 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.9.73', 'volgataski.zapto.org', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['158.160.9.73', 'volgataski.zapto.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': '/data/db.sqlite3',
 #     }
-# } 
+# }
 DATABASES = {
     'default': {
         # Меняем настройку Django: теперь для работы будет использоваться
